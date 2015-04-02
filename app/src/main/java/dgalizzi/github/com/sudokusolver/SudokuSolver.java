dd082b3597ce58b4e8396d7b4d2bc21d54adc4cf
Diego D. Galizzi <dgalizzi@gmail.com>
2015-04-02 02:25:10 -0300

    Clear button functionality.

--- app/src/main/java/dgalizzi/github/com/sudokusolver/SudokuSolver.java
+++ app/src/main/java/dgalizzi/github/com/sudokusolver/SudokuSolver.java
@@ -1,5 +1,6 @@
 package dgalizzi.github.com.sudokusolver;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,14 +11,19 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 
-public class SudokuSolver extends ActionBarActivity {
+public class SudokuSolver extends ActionBarActivity implements View.OnClickListener {
 
     Button mButtons[][];
+    Button mClearButton;
+    Button mSolveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku_solver);
+
+        mClearButton = (Button) findViewById(R.id.clear_button);
+        mSolveButton = (Button) findViewById(R.id.solve_button);
 
         int[] linear_layout_ids = {R.id.buttons_1, R.id.buttons_2, R.id.buttons_3,
                                    R.id.buttons_4, R.id.buttons_5, R.id.buttons_6,
@@ -34,12 +40,34 @@ public class SudokuSolver extends ActionBarActivity {
         {
             for (int j = 0; j < 9; j ++) {
                 Button b = (Button) ((ViewGroup) v[i]).getChildAt(j);
-                //b.setText(String.valueOf(j + 1));
-                b.setText("");
+                b.setText(String.valueOf(i) + '_' + String.valueOf(j));
+                //b.setText("");
 
                 mButtons[i][j] = b;
             }
         }
+        //onClear();
+
+        mClearButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.clear_button:
+                onClear();
+                break;
+            case R.id.solve_button:
+                break;
+        }
+    }
+
+    public void onClear() {
+        for (int i = 0; i < 9; i ++) {
+            for (int j = 0; j < 9; j++) {
+                mButtons[i][j].setText("");
+            }
+        }
+
     }
 
 
